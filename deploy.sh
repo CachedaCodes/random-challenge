30b7e4e6e9b79b7b1fa83fcb896df2817119aa01
CachedaCodes <cachedacodes@gmail.com>
2022-02-19 13:05:18 +0100

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:CachedaCodes/random-challenge.git main:gh-pages
+git push -f git@github.com:CachedaCodes/random-challenge.git HEAD:gh-pages
 cd -
